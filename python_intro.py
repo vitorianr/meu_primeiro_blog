3ef8ff73ebd282bf4548a4ece4364ebf5fc3f794
vitorianr <vnrech@gmail.com>
2020-10-21 15:23:15 -0300

    Minha aplicação Django Girls, first commit

---
+++ python_intro.py
@@ -0,0 +1,32 @@
+'''
+nomes = ['a','b','c','d','e']
+for nome in nomes:
+    print('ola,',nome +'!')
+'''
+'''
+def hi(): 
+    print('olá!)
+    print('tudo bem')
+hi()
+'''
+'''
+for i in range(1,6):
+    print(i)
+'''
+'''
+def hi():
+    print('ola')
+    print('tudo bem')
+hi()
+'''
+def hi(name):
+    if name == 'ola':
+        print('ola ola')
+    elif name == 'sonja':
+        print('ola sonja')
+    else:
+        print('ola estranho')
+hi('abacaxi')
+def hi(name):
+    print('ola'+ name + '!')
+hi(' rachel')
